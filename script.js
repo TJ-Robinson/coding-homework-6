c56efcacb5a80682aaa0dd7a2ea63bd5cc1a8e49
Tyler <robinsontj15@gmail.com>
2022-10-27 14:28:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+fetchqueryURL('https://api.openweathermap.org/data/2.5/forecast?lat={39.9284}&lon={-75.2738}&appid={APIKey}')
+var APIKey = "30c17a45bd00203f770a8a0883d183f1";
+var city = 'upper darby'
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
